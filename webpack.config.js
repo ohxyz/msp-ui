2492a85482c7bad80140d2d29f843eb916c33b7b
Busheng Xi <busheng.xi@originenergy.com.au>
2018-04-03 16:11:46 +1000

    Added webpack dependencies and configuration and some test code

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+var path = require( 'path' );
+
+module.exports = {
+
+    entry: './src/app.js',
+
+    output: {
+
+        path: path.join( __dirname , 'dist' ),
+
+        filename: 'bundle.js'
+    },
+
+    module: {
+        
+        rules: [ 
+
+            {
+
+                test: /\.js[x]{0,1}$/,
+
+                exclude: /(node_modules|bower_components)/,
+
+                use: {
+
+                    loader: 'babel-loader',
+
+                    options: {
+
+                        presets: [ 'react', 'env' ]
+                    }
+                }
+
+            }, 
+
+            { 
+
+                test: /\.less$/,
+
+                use: [ 
+
+                    { loader: "style-loader" },
+                    { loader: "css-loader" },
+                    { loader: "less-loader" }
+                ]
+            } 
+        ]
+    }
+};
