ac6c9fa75a7591f9196fd0b1012e560708fe0361
Busheng Xi <busheng.xi@originenergy.com.au>
2018-04-10 13:06:52 +1000

    Added less examples for demonstration of the layout

--- test/dev/components/search-box/search-box.js
+++ test/dev/components/search-box/search-box.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBox from '../../../../src/components/search-box/search-box.js';
 
-
+require( '../../../../less/components/search-box.less' );
 
 ReactDOM.render(
 
